e5ada501003e825df267d9844d2d516aa9566ca3
lapzerin82-ux <lapzerin82@gmail.com>
2026-01-03 22:17:54 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'hevi-calc-v1';
+const urlsToCache = [
+    '/Heevi-Calculator/',
+    '/Heevi-Calculator/index.html',
+    '/Heevi-Calculator/kurdish_flag.png'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
